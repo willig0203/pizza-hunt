47c3411b470ef2c8303352a37ba3508810309c3f
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-08-23 13:57:36 -0500

    reply functionality  working as expected

--- controllers/routes/api/comment-routes.js
+++ controllers/routes/api/comment-routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../../controllers/routes/comment-controller");
 
 // /api/comments/<pizzaId>
@@ -9,5 +11,9 @@ router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
 router.route("/:pizzaId/:commentId").delete(removeComment);
+
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
